86dec0eb256188911334cccfbd10dbe1f1e1c7ab
Romy <161659316+romygou@users.noreply.github.com>
2024-02-28 16:09:38 -0800

    Update Card.h

--- Card.h
+++ Card.h
@@ -2,8 +2,6 @@
 //  Card.h
 //  Blackjack 
 //
-//  Created by Romy Gou on 10/11/23.
-//
 
 #ifndef CARD
 #define CARD
@@ -15,7 +13,6 @@ namespace cs32
 {
 
 // this class represents a single playing card in a standard deck of playing cards with a Suit and Face value
-// although this class builds and runs, various TODO operations need to be completed by CS 32 students
 class Card
 {
 public:
@@ -27,17 +24,14 @@ public:
     
     // getter for the Suit field of this class
     Suit getSuit() const;
-    // TODO CS 32 students need to complete this operation
     // setter for the Suit field of this class
     void setSuit( Suit s );
     
     // getter for the Face field of this class
     Face getFace() const;
-    // TODO CS 32 students need to complete this operation
     // setter for the Face field of this class
     void setFace( Face f );
     
-    // TODO CS 32 students need to complete this operation
     // determines the numeric equivalent of this playing card based on blackjack rules of play; Aces should be worth one and Tens, Jacks, Queens and Kings should be worth ten
     int  getValue() const;
 private:
